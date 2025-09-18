78f69f422c6ce00132ae6ceb2b5707f9ae54e83d
ArqamShabir <mrking6a@gmail.com>
2025-09-18 14:49:54 +0500

    New Version

--- next.config.js
+++ next.config.js
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   async headers() {
     return [
       {
         source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET",
-          },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
           {
             key: "Access-Control-Allow-Headers",
             value:
